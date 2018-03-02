13ed346256f26ee747bfb5ad2169066437232105
Joey Dumont <joey.dumont@gmail.com>
2018-03-02 16:26:17 -0500

    Draft README. Test with MaxwellVerification.

---
+++ README.md
@@ -0,0 +1,9 @@
+## How to Make the Thesis
+
+ - `git submodule update --init --recursive`: to make all submodules available and current.
+ - Run `make`.
+ - To make the binaries associated with this thesis from scratch
+   + Download the contents of the AWS Bucket associated with the thesis.
+   + Run `make images`.
+
+## License
--- light_table/SolveMaxwell/MaxwellVerification.py
+++ light_table/SolveMaxwell/MaxwellVerification.py
@@ -24,14 +24,15 @@ import scipy.constants as cst
 import math
 import h5py
 from scipy import interpolate
+import scipy.integrate as integrate
 
 # -- OS and other stuff.
 import time
 import argparse
 import imp
 
-# -- Load our custom module.
-vphys = imp.load_source('vphys', "../../python-tools/vphys.py")
+# -- Load our custom modules.
+vphys           = imp.load_source('vphys', "../../python-tools/vphys.py")
 AnalysisStratto = imp.load_source('AnalysisStratto', "../../python-tools/AnalysisStrattoCalculator.py")
 import vphys
 import AnalysisStratto as analstrat
@@ -51,13 +52,94 @@ parser.add_argument("--output-directory",
                       help="Directory where the figures will be saved.")
 args = parser.parse_args()
 
-# ------------------------------ MAIN FUNCTION ------------------------------ #
+# --------------------------- Function Definition --------------------------- #
+def GaussIntegrals(analysis_obj, field_type, r_index, frequency_index):
+  """
+  Evaluates the Gaussian integral of Maxwell's equations, either for the
+  magnetic or electric field, for a given frequency, and for a given value of r.
+  """
+  if field_type == "electric":
+    radial_field = "Er"
+    long_field   = "Ez"
+  elif field_type == "magnetic":
+    radial_field = "Br"
+    long_field  = "Bz"
 
+  # -- Compute the integrands in cylindrical coordinates.
+  Fr = analysis_obj.GetFrequencyComponent(radial_field, frequency_index)
+  Fz = analysis_obj.GetFrequencyComponent(long_field,   frequency_index)
+
+  cylinderIntegrand = np.zeros_like(Fr)
+  circleIntegrand   = np.zeros_like(Fr)
+
+  # -- In cylindrical coordinates, the integrand must be multiplied by r.
+  for i in range(cylinderIntegrand.shape[0]):
+    cylinderIntegrand[i,:,:] = analysis_obj.coord_r[i]*Fr[i,:,:]
+    circleIntegrand[i,:,:]   = -analysis_obj.coord_r[i]*Fz[i,:,:]
+
+  cylinderContribution  =  integrate.simps(integrate.simps(cylinderIntegrand[r_index,:,:],
+                                           x=analysis_obj.coord_z[:]),
+                                           x=analysis_obj.coord_theta[:])
+
+  negCircleContribution = -integrate.simps(integrate.simps(circleIntegrand[0:r_index-1,:,0],
+                                           x=analysis_obj.coord_theta[:]),
+                                           x=analysis_obj.coord_r[0:r_index-1])
+
+  posCircleContribution = integrate.simps(integrate.simps(circleIntegrand[0:r_index-1,:,-1],
+                                           x=analysis_obj.coord_theta[:]),
+                                           x=analysis_obj.coord_r[0:r_index-1])
+
+  return cylinderContribution+negCircleContribution+posCircleContribution
+
+def SurfaceLineIntegrals(analysis_obj, field_type, r_index, z_index, frequency_index):
+  """
+  Evaluates the flux thorugh a circular surface, and the current over the loop.
+  """
+  if field_type == "electric":
+    factor = 1
+    theta_field = "Eth"
+    long_field   = "Bz"
+  elif field_type == "magnetic":
+    factor = -1
+    radial_field = "Bth"
+    long_field  = "Ez"
+
+  Fth = analysis_obj.GetFrequencyComponent(theta_field, frequency_index)
+  Fz  = analysis_obj.GetFrequencyComponent(long_field, frequency_index)
+
+  lineIntegrand   = np.zeros_like(Fz)
+  circleIntegrand = np.zeros_like(Fz)
+
+  # -- In cylindrical coordinates, the integrand must be multiplied by r.
+  for i in range(lineIntegrand.shape[0]):
+    lineIntegrand[i,:,:]   = analysis_obj.coord_r[i]*Fth[i,:,:]
+    circleIntegrand[i,:,:] = analysis_obj.coord_r[i]*Fz[i,:,:]
+
+  lineContribution   = integrate.simps(lineIntegrand[r_index,:,z_index],
+                                       x=analysis_obj.coord_theta[:])
+  circleContribution = integrate.simps(integrate.simps(circleIntegrand[0:r_index-1,:,z_index],
+                                       x=analysis_obj.coord_theta[:]),
+                                       x=analysis_obj.coord_r[0:r_index-1])
+
+  return lineContribution-1j*analysis_obj.omega[frequency_index]*analysis_obj.UNIT_TIME*factor*circleContribution
+
+# ------------------------------ MAIN FUNCTION ------------------------------ #
 # -- Open the file for analysis.
 analysis_object = analstrat.Analysis3D(freq_field="../../bin/MaxwellVerification/Field_reflected.hdf5")
 
-# -- Evaluate the surface integral at specified values of r.
+print("The surface integral for the electric case yields: {}".format(GaussIntegrals(analysis_object,
+                                                                                    "electric",
+                                                                                    analysis_object.size_r//2,
+                                                                                    analysis_object.size_freq//2)))
 
-# -- Evaluate the line integral at specified values of r.
+print("The surface integral for the electric case yields: {}".format(GaussIntegrals(analysis_object,
+                                                                                    "magnetic",
+                                                                                    analysis_object.size_r//2,
+                                                                                    analysis_object.size_freq//2)))
 
-# -- Evaluate the line integrals at specified values of r.
+print("The line integral for the electric case yields: {}".format(SurfaceLineIntegrals(analysis_object,
+                                                                                       "electric",
+                                                                                       analysis_object.size_r//2,
+                                                                                       analysis_object.size_z//2,
+                                                                                       analysis_object.size_freq//2)))
+
